2dabd62533069f5d74cae438449efdd5759449e3
martinval20 <martinval2000@hotmail.com>
2023-11-13 13:52:07 -0500

    chat-component

--- rds-v1/src/components/common/Chat/Search.jsx
+++ rds-v1/src/components/common/Chat/Search.jsx
@@ -1,20 +1,60 @@
-import React from "react";
+//CHECH THIS LEATER
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../../../firebaseConfig";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(firestore, "users"),
+      where("name", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+    console.log(username);
+    console.log(username.user);
+  };
+
+  const handleSelect = async ()=>{
+    //Check the group (chats in firestore)exists, if not then create
+    const res= await getDocs(firestore, "chats")
+
+    //create user chats
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Buscar contacto" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYEfJo6j7axefgRh1oPdPgcwOBc8PeIw8LHR6QXMlosA&s"
-          alt=""
+        <input
+          type="text"
+          placeholder="Buscar contacto"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <div className="userChatInfo">
-          <span>Martin</span>
-        </div>
       </div>
+      {err && <span>Usuario no encontrado</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user.imageLink} alt="" />
+          <div className="userChatInfo">
+            <span>{user.name} {user.lastname}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- rds-v1/src/firebaseConfig.js
+++ rds-v1/src/firebaseConfig.js
@@ -5,8 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Your web app's Firebase configurations
 const firebaseConfig = {
   apiKey: "AIzaSyAuKQqRCLq0nBCmF_2dQ7s5In4fXTejhbU",
   authDomain: "redservice-e3880.firebaseapp.com",
@@ -18,6 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 //Variable app is used to passing the firebaseConfig data
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
